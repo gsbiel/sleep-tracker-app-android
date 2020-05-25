9a996c1eebc765b91d297dd2e2a32256a81bf117
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-05-25 09:17:39 -0300

    implement data binding to connect ViewHolder to layout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.android.trackmysleepquality.sleeptracker
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
@@ -11,6 +10,7 @@ import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
 class SleepTrackerAdapter : ListAdapter<SleepNight, SleepTrackerAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
@@ -40,11 +40,11 @@ class SleepTrackerAdapter : ListAdapter<SleepNight, SleepTrackerAdapter.ViewHold
         return ViewHolder.from(parent)
     }
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(val binding: ListItemSleepNightBinding) : RecyclerView.ViewHolder(binding.root){
 
-        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
-        val quality: TextView = itemView.findViewById(R.id.quality_string)
-        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+        val sleepLength: TextView = binding.sleepLength
+        val quality: TextView = binding.qualityString
+        val qualityImage: ImageView = binding.qualityImage
 
         fun bind(item: SleepNight) {
             val res = itemView.context.resources
@@ -65,24 +65,22 @@ class SleepTrackerAdapter : ListAdapter<SleepNight, SleepTrackerAdapter.ViewHold
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater
-                        .inflate(R.layout.list_item_sleep_night, parent, false)
-                return ViewHolder(view)
+//                val view = layoutInflater
+//                        .inflate(R.layout.list_item_sleep_night, parent, false)
+                val binding = ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
     }
 }
 
 class SleepNightDiffCallback: DiffUtil.ItemCallback<SleepNight>() {
-
     override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
         return oldItem.nightId == newItem.nightId
     }
-
     override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
         // SleepNight é um DataClass, logo, podemos usar o operador de igualdade para saber se dois objetos são iguais
         // Isto é, se ambos possuem propriedades com os mesmos valores.
         return oldItem == newItem
     }
-
 }
